4dafd691a20237b5c471786ed9eaa96a76ab3bcc
EthanOlivier <122174496+EthanOlivier@users.noreply.github.com>
2024-03-02 17:59:49 -0500

    Add project files.

---
+++ Mancala Backup/Mancala/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnPOne0 = New Button()
+        btnPOne1 = New Button()
+        btnPOne2 = New Button()
+        btnPOne4 = New Button()
+        btnPOne5 = New Button()
+        btnPOne3 = New Button()
+        lblPOneScore = New Label()
+        lblPTwoScore = New Label()
+        btnPTwo12 = New Button()
+        btnPTwo11 = New Button()
+        btnPTwo10 = New Button()
+        btnPTwo9 = New Button()
+        btnPTwo8 = New Button()
+        btnPTwo7 = New Button()
+        lblPlayerTurn = New Label()
+        lblSpecial = New Label()
+        SuspendLayout()
+        ' 
+        ' btnPOne0
+        ' 
+        btnPOne0.BackColor = SystemColors.Control
+        btnPOne0.Location = New Point(130, 241)
+        btnPOne0.Name = "btnPOne0"
+        btnPOne0.Size = New Size(75, 75)
+        btnPOne0.TabIndex = 0
+        btnPOne0.Text = "4"
+        btnPOne0.UseVisualStyleBackColor = False
+        ' 
+        ' btnPOne1
+        ' 
+        btnPOne1.BackColor = SystemColors.Control
+        btnPOne1.Location = New Point(210, 241)
+        btnPOne1.Name = "btnPOne1"
+        btnPOne1.Size = New Size(75, 75)
+        btnPOne1.TabIndex = 0
+        btnPOne1.Text = "4"
+        btnPOne1.UseVisualStyleBackColor = False
+        ' 
+        ' btnPOne2
+        ' 
+        btnPOne2.BackColor = SystemColors.Control
+        btnPOne2.Location = New Point(292, 241)
+        btnPOne2.Name = "btnPOne2"
+        btnPOne2.Size = New Size(75, 75)
+        btnPOne2.TabIndex = 0
+        btnPOne2.Text = "4"
+        btnPOne2.UseVisualStyleBackColor = False
+        ' 
+        ' btnPOne4
+        ' 
+        btnPOne4.BackColor = SystemColors.Control
+        btnPOne4.Location = New Point(454, 241)
+        btnPOne4.Name = "btnPOne4"
+        btnPOne4.Size = New Size(75, 75)
+        btnPOne4.TabIndex = 0
+        btnPOne4.Text = "4"
+        btnPOne4.UseVisualStyleBackColor = False
+        ' 
+        ' btnPOne5
+        ' 
+        btnPOne5.BackColor = SystemColors.Control
+        btnPOne5.Location = New Point(535, 241)
+        btnPOne5.Name = "btnPOne5"
+        btnPOne5.Size = New Size(75, 75)
+        btnPOne5.TabIndex = 0
+        btnPOne5.Text = "4"
+        btnPOne5.UseVisualStyleBackColor = False
+        ' 
+        ' btnPOne3
+        ' 
+        btnPOne3.BackColor = SystemColors.Control
+        btnPOne3.Location = New Point(373, 241)
+        btnPOne3.Name = "btnPOne3"
+        btnPOne3.Size = New Size(75, 75)
+        btnPOne3.TabIndex = 0
+        btnPOne3.Text = "4"
+        btnPOne3.UseVisualStyleBackColor = False
+        ' 
+        ' lblPOneScore
+        ' 
+        lblPOneScore.BackColor = SystemColors.ActiveCaption
+        lblPOneScore.BorderStyle = BorderStyle.FixedSingle
+        lblPOneScore.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        lblPOneScore.Location = New Point(617, 110)
+        lblPOneScore.Name = "lblPOneScore"
+        lblPOneScore.Size = New Size(100, 163)
+        lblPOneScore.TabIndex = 1
+        lblPOneScore.Text = "0"
+        lblPOneScore.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblPTwoScore
+        ' 
+        lblPTwoScore.BackColor = SystemColors.ActiveCaption
+        lblPTwoScore.BorderStyle = BorderStyle.FixedSingle
+        lblPTwoScore.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        lblPTwoScore.Location = New Point(24, 110)
+        lblPTwoScore.Name = "lblPTwoScore"
+        lblPTwoScore.Size = New Size(100, 163)
+        lblPTwoScore.TabIndex = 1
+        lblPTwoScore.Text = "0"
+        lblPTwoScore.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnPTwo12
+        ' 
+        btnPTwo12.BackColor = SystemColors.Control
+        btnPTwo12.BackgroundImageLayout = ImageLayout.None
+        btnPTwo12.Enabled = False
+        btnPTwo12.Location = New Point(130, 69)
+        btnPTwo12.Name = "btnPTwo12"
+        btnPTwo12.Size = New Size(75, 75)
+        btnPTwo12.TabIndex = 0
+        btnPTwo12.Text = "4"
+        btnPTwo12.UseVisualStyleBackColor = False
+        ' 
+        ' btnPTwo11
+        ' 
+        btnPTwo11.BackColor = SystemColors.Control
+        btnPTwo11.Enabled = False
+        btnPTwo11.Location = New Point(210, 69)
+        btnPTwo11.Name = "btnPTwo11"
+        btnPTwo11.Size = New Size(75, 75)
+        btnPTwo11.TabIndex = 0
+        btnPTwo11.Text = "4"
+        btnPTwo11.UseVisualStyleBackColor = False
+        ' 
+        ' btnPTwo10
+        ' 
+        btnPTwo10.BackColor = SystemColors.Control
+        btnPTwo10.Enabled = False
+        btnPTwo10.Location = New Point(292, 69)
+        btnPTwo10.Name = "btnPTwo10"
+        btnPTwo10.Size = New Size(75, 75)
+        btnPTwo10.TabIndex = 0
+        btnPTwo10.Text = "4"
+        btnPTwo10.UseVisualStyleBackColor = False
+        ' 
+        ' btnPTwo9
+        ' 
+        btnPTwo9.BackColor = SystemColors.Control
+        btnPTwo9.Enabled = False
+        btnPTwo9.Location = New Point(373, 69)
+        btnPTwo9.Name = "btnPTwo9"
+        btnPTwo9.Size = New Size(75, 75)
+        btnPTwo9.TabIndex = 0
+        btnPTwo9.Text = "4"
+        btnPTwo9.UseVisualStyleBackColor = False
+        ' 
+        ' btnPTwo8
+        ' 
+        btnPTwo8.BackColor = SystemColors.Control
+        btnPTwo8.Enabled = False
+        btnPTwo8.Location = New Point(454, 69)
+        btnPTwo8.Name = "btnPTwo8"
+        btnPTwo8.Size = New Size(75, 75)
+        btnPTwo8.TabIndex = 0
+        btnPTwo8.Text = "4"
+        btnPTwo8.UseVisualStyleBackColor = False
+        ' 
+        ' btnPTwo7
+        ' 
+        btnPTwo7.BackColor = SystemColors.Control
+        btnPTwo7.Enabled = False
+        btnPTwo7.Location = New Point(535, 69)
+        btnPTwo7.Name = "btnPTwo7"
+        btnPTwo7.Size = New Size(75, 75)
+        btnPTwo7.TabIndex = 0
+        btnPTwo7.Text = "4"
+        btnPTwo7.UseVisualStyleBackColor = False
+        ' 
+        ' lblPlayerTurn
+        ' 
+        lblPlayerTurn.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        lblPlayerTurn.Location = New Point(12, 332)
+        lblPlayerTurn.Name = "lblPlayerTurn"
+        lblPlayerTurn.Size = New Size(716, 40)
+        lblPlayerTurn.TabIndex = 2
+        lblPlayerTurn.Text = "Player One Turn!"
+        lblPlayerTurn.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblSpecial
+        ' 
+        lblSpecial.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        lblSpecial.Location = New Point(12, 372)
+        lblSpecial.Name = "lblSpecial"
+        lblSpecial.Size = New Size(716, 40)
+        lblSpecial.TabIndex = 2
+        lblSpecial.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' frmMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.AppWorkspace
+        ClientSize = New Size(740, 428)
+        Controls.Add(lblSpecial)
+        Controls.Add(lblPlayerTurn)
+        Controls.Add(btnPTwo7)
+        Controls.Add(btnPTwo8)
+        Controls.Add(btnPTwo9)
+        Controls.Add(btnPTwo10)
+        Controls.Add(btnPTwo11)
+        Controls.Add(btnPTwo12)
+        Controls.Add(lblPTwoScore)
+        Controls.Add(lblPOneScore)
+        Controls.Add(btnPOne3)
+        Controls.Add(btnPOne5)
+        Controls.Add(btnPOne4)
+        Controls.Add(btnPOne2)
+        Controls.Add(btnPOne1)
+        Controls.Add(btnPOne0)
+        MaximizeBox = False
+        Name = "frmMain"
+        Text = "Mancala"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnPOne0 As Button
+    Friend WithEvents btnPOne1 As Button
+    Friend WithEvents btnPOne2 As Button
+    Friend WithEvents btnPOne4 As Button
+    Friend WithEvents btnPOne5 As Button
+    Friend WithEvents btnPOne3 As Button
+    Friend WithEvents lblPOneScore As Label
+    Friend WithEvents lblPTwoScore As Label
+    Friend WithEvents btnPTwo12 As Button
+    Friend WithEvents btnPTwo11 As Button
+    Friend WithEvents btnPTwo10 As Button
+    Friend WithEvents btnPTwo9 As Button
+    Friend WithEvents btnPTwo8 As Button
+    Friend WithEvents btnPTwo7 As Button
+    Friend WithEvents lblPlayerTurn As Label
+    Friend WithEvents lblSpecial As Label
+End Class
